5537187f8c85deca395ebefd369dec8629a45923
Russell Purinton <russell.purinton@gmail.com>
2023-08-19 21:15:38 -0400

    Revert "Update InboxHandler.php"
    This reverts commit 1fcb29c09975ec65148a69c277175c54d5590552.

--- src/InboxHandler.php
+++ src/InboxHandler.php
@@ -21,7 +21,6 @@ class InboxHandler extends ConfigLoader
 
     private function process($message)
     {
-        echo ("Processing " . print_r($message, true) . "\n");
         $this->promptwriter->query("SELECT 1");
         try {
             $this->log_incomming($message);
