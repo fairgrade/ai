e58a68b2c376310fe1e99d36b47f9f2c1b7f3e69
Russell Purinton <russell.purinton@gmail.com>
2023-08-20 03:50:14 -0400

    Update openings.php
    testing different approach

--- src/cron.d/openings.php
+++ src/cron.d/openings.php
@@ -3,17 +3,51 @@
 
 namespace fairgrade\ai;
 
+require_once(__DIR__ . "/PromptWriter.php");
+$pw = new PromptWriter();
 
-$publish_message = json_decode(file_get_contents(__DIR__ . '/template.json'), true);
-$microtime = number_format(microtime(true), 6, '.', '');
-$publish_message["d"]["microtime"] = -1;
-$publish_message["d"]["id"] = 1142616719630803116;
-$publish_message["d"]["bot_id"] = 1142616719630803116;
-$publish_message["d"]["channel_id"] = 1142706861733318707;
-$publish_message["d"]["channel_name"] = "#openings";
-$publish_message["d"]["channel_topic"] = "Current Job Openings";
-$publish_message["d"]["content"] = "Pick one of the current openings to write a discord post about.  mention 2 others briefly.";
-publish("ai_inbox", $publish_message);
+try {
+    extract($pw->single("SELECT `server_prompt` FROM `discord_servers` WHERE `server_id` = 1142328958399025253"));
+    $messages[] = ["role" => "system", "content" => $server_prompt];
+    $messages[] = ["role" => "user", "content" => "Pick one of the current openings to write a discord post in about 1600 characters.  mention 2 others briefly. use markdown and emojis."];
+    $model = 'gpt-3.5-turbo-0613';
+    $prompt = [
+        'model' => $model,
+        'messages' => $messages,
+        'temperature' => 0.986,
+        'top_p' => 0.986,
+        'frequency_penalty' => 0,
+        'presence_penalty' => 0
+    ];
+    $log_id = $this->promptwriter->startChatGPT("Discord Bots", $prompt, 1142616719630803116);
+    $stream = $this->promptwriter->openai->chat()->createStreamed($prompt);
+    $full_response = "";
+    echo (6);
+    foreach ($stream as $response) {
+        $reply = $response->choices[0]->toArray();
+        if (isset($reply["delta"]["content"])) {
+            $delta_content = $reply["delta"]["content"];
+            $full_response .= $delta_content;
+            if (microtime(true) > $typing_time) {
+                $this->start_typing($message);
+                $typing_time = microtime(true) + 4;
+            }
+        }
+    }
+    echo (7);
+    $this->promptwriter->endChatGPT($log_id, $full_response);
+    echo (8);
+    if (substr($full_response, 0, 1) == "[" && strpos($full_response, "]") == 6) $full_response = substr($full_response, 8);
+    if (substr($full_response, 0, strlen($bot_name) + 2) == $bot_name . ": ") $full_response = substr($full_response, strlen($bot_name) + 2);
+} catch (\Exception $e) {
+    echo (9);
+    $this->promptwriter->errorChatGPT($log_id, print_r($e, true));
+    print_r($e);
+    if (!isset($full_response) || $full_response == "") $full_response = "I'm sorry, but " . $e->getMessage() . "\n";
+    else $full_response .= "\n\nAlso, I'm sorry, but " . $e->getMessage() . "\n";
+}
+
+echo ($full_respone . "\n");
 
 function publish($queue, $data)
 {
