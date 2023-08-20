6af03d8d3e5dfe8be626bf15b58d395d272e7ae0
Russell Purinton <russell.purinton@gmail.com>
2023-08-20 04:07:48 -0400

    Update openings.php
    correcting behavior

--- src/cron.d/openings.php
+++ src/cron.d/openings.php
@@ -9,7 +9,11 @@ $pw = new PromptWriter();
 try {
     extract($pw->single("SELECT `server_prompt` FROM `discord_servers` WHERE `server_id` = 1142328958399025253"));
     $messages[] = ["role" => "system", "content" => $server_prompt];
-    $messages[] = ["role" => "user", "content" => "Pick one of the current openings randomly to write a discord post in about 1600 characters.  mention 2 others briefly. use markdown and emojis."];
+    $messages[] = ["role" => "user", "content" => "
+    Randomly pick one of the current openings randomly to write a discord post in about 1500 characters.
+    Randomly pick 2 others to mention briefly.
+    use markdown and emojis in your post.
+    the call to action is to ask you more in their own private channel."];
     $model = 'gpt-3.5-turbo-0613';
     $prompt = [
         'model' => $model,
